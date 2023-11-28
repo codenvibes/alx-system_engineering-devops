72ac0188561875cc024d2ad83a767ef3aea9fed4
codenvibes <terrymk1999@gmail.com>
2023-11-28 13:40:35 +0300

    Feat: Create a file in /tmp using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# AUTH: codenvibes
+# DESC:
+
+file {'/tmp/school':
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data'
+    group   => 'www-data',
+    content => 'I love Puppet'
+}
