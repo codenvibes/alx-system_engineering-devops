876ecfb00b3a198c0c2b7553fb1cd1edd5f2ea38
codenvibes <terrymk1999@gmail.com>
2023-11-28 18:40:24 +0300

    Create a puppet manisfest that installs flask 2.1.0 from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,5 @@
-# a puppet manisfest that installs flask 2.1.0 from pip3
+# AUTH: codenvibes
+# DESC: a puppet manisfest that installs flask 2.1.0 from pip3
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
