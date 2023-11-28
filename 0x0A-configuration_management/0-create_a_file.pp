0387f8cb1cd4078b8ce99dddfa87a0e2f823b821
codenvibes <terrymk1999@gmail.com>
2023-11-28 15:03:22 +0300

    Feat: Create a file in /tmp using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,5 @@
-# AUTH: codenvibes, 
-# DESC: a puppet manifest that creates a file in /tmp
+# AUTH: codenvibes, DESC: a puppet manifest that creates a file in /tmp
+
 file {'/tmp/school':
     ensure  => present,
     mode    => '0744',
