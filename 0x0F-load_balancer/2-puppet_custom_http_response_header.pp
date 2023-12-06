6ff6cd516983f08a9662b282a9604f7bcf4e7041
codenvibes <terrymk1999@gmail.com>
2023-12-06 05:14:28 +0300

    Chore: Create a puppet manifest that Configures Nginx with Custom Header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,6 @@
+# AUTH: codenvibes
+# DESC: A Puppet manifest that Configures Nginx with Custom Header.
+#       This Puppet manifest updates package information, installs the Nginx package,
+#       creates a basic HTML file, configures Nginx to include a custom HTTP header,
+#       and ensures the Nginx service is running.
+
